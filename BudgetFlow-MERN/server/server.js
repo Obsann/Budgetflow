4a5abf644c8fb77877d4b06e4f9f4832b7c557e6
Obsann <obsanhabtamu0@gmail.com>
2026-02-06 16:15:53 +0300

    server app update cors link to vercel link

--- BudgetFlow-MERN/server/server.js
+++ BudgetFlow-MERN/server/server.js
@@ -13,9 +13,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
-    credentials: true
+  origin: "https://budgetflow-524f.vercel.app",
+  credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
